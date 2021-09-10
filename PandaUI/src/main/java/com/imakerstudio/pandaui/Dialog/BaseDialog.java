c8dc1a2181102892f062b1eb70b52e0a51b7c89a
MBP_jagger <sohm187@gmail.com>
2021-09-10 16:11:48 +0800

    Update dependencies

--- PandaUI/src/main/java/com/imakerstudio/pandaui/Dialog/BaseDialog.java
+++ PandaUI/src/main/java/com/imakerstudio/pandaui/Dialog/BaseDialog.java
@@ -2,10 +2,10 @@ package com.imakerstudio.pandaui.Dialog;
 
 import android.app.Dialog;
 import android.content.Context;
-import androidx.annotation.NonNull;
 
 public class BaseDialog extends Dialog {
-  public BaseDialog(@NonNull Context context) {
+
+  public BaseDialog(Context context) {
     super(context);
   }
 }
